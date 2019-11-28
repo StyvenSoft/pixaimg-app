359d96629dae3b52836cd36c017b0d6cdff15637
Steveen <steveen.echeverri@gmail.com>
2019-11-27 22:31:27 -0500

    Url console search Api

--- src/App.js
+++ src/App.js
@@ -3,8 +3,22 @@ import Searcher from './components/Searcher';
 
 class App extends Component{
 
+  state = {
+    term : ''
+  }
+
+  searchApi = () =>{
+    const url = `https://pixabay.com/api/?key=14436935-576eb479a35a06e9082a2ddf4&q=${this.state.term}`;
+    console.log(url);
+  }
+
   dataSearch = (term) => {
-    console.log(term)
+    //console.log(term)
+    this.setState({
+      term
+    }, () =>{
+      this.searchApi();
+    })
   }
   render() {
 
@@ -14,10 +28,10 @@ class App extends Component{
             <p className="lead text-center">Buscar Imagen..</p>
             <Searcher dataSearch={this.dataSearch}/>
           </div>
+          { this.state.term }
       </div>
     );
   }
 }
-
 
 export default App;
