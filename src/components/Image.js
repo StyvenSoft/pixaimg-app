75494d060df8fc738b4f27d67ffd5b51f05f70e9
Steveen <steveen.echeverri@gmail.com>
2020-01-04 22:06:17 -0500

    Properties card body

--- src/components/Image.js
+++ src/components/Image.js
@@ -4,12 +4,17 @@ import React from 'react';
 
 const image = (props) =>{
 
-    const { previewURL, tags } = props.image;
+    const { previewURL, tags, largeImageURL, likes, views } = props.image;
 
     return (
         <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
             <div className="card">
                 <img src={previewURL} alt={tags} className="card-img-top" />
+                <div className="card-body">
+                    <p className="card-text">{ likes } Me gusta</p>
+                    <p className="card-text">{ views } Vistas</p>
+                    <a href={largeImageURL} target="_blank" rel="noopener noreferrer" className="btn btn-primary btn-block">Ver imagen</a>
+                </div>
             </div>
         </div>
     )
